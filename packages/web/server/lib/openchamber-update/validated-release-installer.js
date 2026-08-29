3bed0819abb8c0703af108ec80847c10f6368905
James Korzekwa <jkorzekwa@j2kmini.local>
2026-08-29 01:47:41 -0600

    Canonicalize managed rollback targets

--- packages/web/server/lib/openchamber-update/validated-release-installer.js
+++ packages/web/server/lib/openchamber-update/validated-release-installer.js
@@ -725,7 +725,7 @@ async function getLinkTarget(linkPath) {
   try {
     const stat = await fsp.lstat(linkPath);
     if (!stat.isSymbolicLink()) fail(`${linkPath} exists but is not a symbolic link`);
-    return path.resolve(path.dirname(linkPath), await fsp.readlink(linkPath));
+    return await fsp.realpath(linkPath);
   } catch (error) {
     if (error?.code === 'ENOENT') return null;
     throw error;
